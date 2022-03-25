4dc812a4fb2d3f3619fe94d2045c40c37dc8e413
sunami09 <sunamidasgupta@gmail.com>
2022-03-26 02:40:30 +0530

    Pointers

--- Day 001/Pointers.cpp
+++ Day 001/Pointers.cpp
@@ -1,11 +1,30 @@
+// Datatype -- int*, float*, string*
+
+// address of a variable -- &p
+
+
+// value of pointer - p -- *p 
+
+
 #include<bits/stdc++.h>
 using namespace std;
  
 int main()
 {
-    int i = 10;
+    // int i = 10;
 
-    cout<<&i<<endl;
+    // cout<<&i<<endl;
+
+    int i = 9;
+    int* p = &i;
+
+    i++;
+    (*p)++;
+    cout << p << endl;
+
+    cout<< *p <<endl;
+
+    cout<< &p <<endl;
 
     return 0;
 }
